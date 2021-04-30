0b4c7278bd235bfb8fab01e3d0c06ee15a7903ce
cama-ka <karlacamarena91@yahoo.fr>
2021-04-30 16:27:22 +0200

    Modification while true

--- script.py
+++ script.py
@@ -4,42 +4,44 @@ def informations():
     slogan = input("Le slogan optionnel : ").capitalize()
     resum = input("Le résumé du projet : ").capitalize()
 
-    inputs = (f"## {title}<br> ### {under_title}<br> {slogan}<br>{resum} <br>")
+    inputs = (f"## {title}\n### {under_title}\n {slogan}\n {resum}\n")
     return inputs
         
 def auteurs():
     auteur = input("Indiquez le prénom et nom de l'auteur : ").title()
     entreprise = input("Indiquez son entreprise : ").capitalize()
-    # auteur_complet = str(f"{auteur}. {entreprise}")
     return f"{auteur}. {entreprise}"
     
 def prerequis():
-    pre_info = input("notez votre pré-requis : ").capitalize()
-    prerequis_infos = (f"* {pre_info}<br>")
+    pre_info = input("Notez votre pré-requis : ").capitalize()
+    prerequis_infos = (f"* {pre_info}\n")
     return prerequis_infos
 
 def compte(variable, func):
-    print(f"Combien {variable} avez-vous à notifier? Tapez un nombre à partir de 0 :  ")
-    number = int(input())
-    i = 1
-    try:
-        while i <= number:
-            i += 1
-            response = func()
-            with open("test.txt","a") as file:
-                file.write(response)
-                file.close()
-    except OSError:
-        print("ça ne fonctionne pas")
+    print(f"Combien {variable} avez-vous à notifier ?")
+    while True:
+        try:
+            number = int(input())
+            i = 1
+            while i <= number:
+                i += 1
+                response = func()
+                with open("README.md","a") as file:
+                    file.write(response)
+                    file.close()
+            break
+        except ValueError:
+            print(f"Vous n'avez pas {variable}.")
+            break
 
 
 def main():
     with open("README.md","w") as file:
         file.write(informations())
         file.close()
-    compte("pré-requis", prerequis)
-    compte("auteurs", auteurs)
-    
+    compte("de pré-requis", prerequis)
+    compte("d' auteurs", auteurs)
+    print("Le fichier est terminé.")
             
 if __name__ == "__main__":
     main()
